937b86757ca006974a6efd7ec44132fc5530bc8d
Lopez <135629199+KazinMDM@users.noreply.github.com>
2025-10-17 08:58:59 -0300

    atualização da main

--- EstoqueMercado/main.py
+++ EstoqueMercado/main.py
@@ -1,11 +1,13 @@
 def menu_principal():
-    opcoes = ["Gerenciar Estoque", "Visulizar Estoque", "Sair"]
+    opcoes = ["Inserir produtos", "Remover produtos", "Atualizar informações do produtos", "Visulizar Estoque", "Buscar produto", "Sair"]
     print("Olá, seja bem vindo ao Sistema de Estoque de Mercado!")
     for i in range(len(opcoes)):
         if opcoes[i] == "Sair":
             print(f"0 - {opcoes[i]}")
         else:
             print(f"{i+1} - {opcoes[i]}")
+    resp = int(input("Digite a opção desejada: "))
+    return resp
 
 if __name__ == '__main__':
     menu_principal() 
