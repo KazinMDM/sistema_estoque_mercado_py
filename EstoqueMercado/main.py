0470fd02a98b66f11b20aecedd9069eb1b44f77d
Lopez <135629199+KazinMDM@users.noreply.github.com>
2025-10-17 08:37:00 -0300

    .

---
+++ EstoqueMercado/main.py
@@ -0,0 +1,11 @@
+def menu_principal():
+    opcoes = ["Gerenciar Estoque", "Visulizar Estoque", "Sair"]
+    print("Olá, seja bem vindo ao Sistema de Estoque Mercado!")
+    for i in range(len(opcoes)):
+        if opcoes[i] == "Sair":
+            print(f"0 - {opcoes[i]}")
+        else:
+            print(f"{i+1} - {opcoes[i]}")
+
+if __name__ == '__main__':
+    menu_principal() 
