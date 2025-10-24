967a1581c5d28e1a971bb0ca3af0ef02a1656422
Lopez <135629199+KazinMDM@users.noreply.github.com>
2025-10-24 09:12:41 -0300

    .

--- EstoqueMercado/inserirRemoverProdutos.py
+++ EstoqueMercado/inserirRemoverProdutos.py
@@ -1,6 +1,7 @@
 import os
 import time
 import json
+from utils import Utils
 
 class Produtos:
     def __init__(self, nome, quantidade, preco, codigo):
@@ -29,14 +30,44 @@ class higiene_pessoal(Produtos):
         self.categoria = categoria
 
 class Carnes(Produtos):
-    def __init__(self, nome, quantidade, preco, codigo, categoria="Carnes"):
+    def __init__(self, nome, quantidade, preco, codigo, categoria="Carnes",tipo_corte=None):
         super().__init__(nome, quantidade, preco, codigo)
         self.categoria = categoria
+        self.tipo_corte = tipo_corte
 
 class Bebidas(Produtos):
     def __init__(self, nome, quantidade, preco, codigo, categoria="Bebidas"):
         super().__init__(nome, quantidade, preco, codigo)
         self.categoria = categoria
 
-def limpar_tela():
-    os.system('cls' if os.name == 'nt' else 'clear')
+def inserir_produto():
+    Utils.limpar_tela()
+    print("=== Inserir Produto ===")
+    nome = input("Digite o nome do produto: ")
+    quantidade = int(input("Digite a quantidade do produto: "))
+    preco = float(input("Digite o preço do produto: "))
+    codigo = input("Digite o código do produto: ")
+    print("Selecione a categoria do produto:")
+    categorias = ["Limpeza", "Alimentos_frescos", "Higiene_pessoal", "Carnes", "Bebidas"]
+    for i in range(len(categorias)):
+        print(f"{i+1} - {categorias[i]}")
+    escolha = int(input("Digite o número da categoria: "))
+    
+    if escolha == 1:
+        produto = Limpeza(nome, quantidade, preco, codigo)
+    elif escolha == 2:
+        produto = Alimentos_frescos(nome, quantidade, preco, codigo)
+    elif escolha == 3:
+        produto = higiene_pessoal(nome, quantidade, preco, codigo)
+    elif escolha == 4:
+        tipo_corte = input("Digite o tipo de corte da carne: ")
+        produto = Carnes(nome, quantidade, preco, codigo, tipo_corte=tipo_corte)
+    elif escolha == 5:
+        produto = Bebidas(nome, quantidade, preco, codigo)
+    else:
+        print("Categoria inválida!")
+        return None
+
+    print("Produto inserido com sucesso!")
+    time.sleep(2)
+    return produto
--- EstoqueMercado/utils.py
+++ EstoqueMercado/utils.py
@@ -16,3 +16,4 @@ class Utils:
     def salvar_dados(caminho_arquivo, dados):
         with open(caminho_arquivo, 'w') as arquivo:
             json.dump(dados, arquivo, indent=4)
+        
