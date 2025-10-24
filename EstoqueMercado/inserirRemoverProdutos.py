7a6f31c4917317fa042a88d2e34dd4f36ed156bc
Lopez <135629199+KazinMDM@users.noreply.github.com>
2025-10-24 08:22:11 -0300

    .

--- EstoqueMercado/inserirRemoverProdutos.py
+++ EstoqueMercado/inserirRemoverProdutos.py
@@ -12,4 +12,31 @@ class Produtos:
     def __str__(self):
         return f"Nome: {self.nome}\nCategoria: {self.categoria}\nQuantidade: {self.quantidade}\nPreco: {self.preco}\nCodigo: {self.codigo}"
     
-class Limpeza:
+
+class Limpeza(Produtos):
+    def __init__(self, nome, quantidade, preco, codigo, categoria="Limpeza"):
+        super().__init__(nome, quantidade, preco, codigo)
+        self.categoria = categoria
+
+class Alimentos_frescos(Produtos):
+    def __init__(self, nome, quantidade, preco, codigo, categoria="Alimentos_frescos"):
+        super().__init__(nome, quantidade, preco, codigo)
+        self.categoria = categoria
+
+class higiene_pessoal(Produtos):
+    def __init__(self, nome, quantidade, preco, codigo, categoria="Higiene_pessoal"):
+        super().__init__(nome, quantidade, preco, codigo)
+        self.categoria = categoria
+
+class Carnes(Produtos):
+    def __init__(self, nome, quantidade, preco, codigo, categoria="Carnes"):
+        super().__init__(nome, quantidade, preco, codigo)
+        self.categoria = categoria
+
+class Bebidas(Produtos):
+    def __init__(self, nome, quantidade, preco, codigo, categoria="Bebidas"):
+        super().__init__(nome, quantidade, preco, codigo)
+        self.categoria = categoria
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
---
+++ EstoqueMercado/utils.py
@@ -0,0 +1,18 @@
+import os
+import time
+import json
+
+class Utils:
+    def limpar_tela():
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def carregar_dados(caminho_arquivo):
+        if not os.path.exists(caminho_arquivo):
+            return {}
+        with open(caminho_arquivo, 'r') as arquivo:
+            dados = json.load(arquivo)
+        return dados
+    
+    def salvar_dados(caminho_arquivo, dados):
+        with open(caminho_arquivo, 'w') as arquivo:
+            json.dump(dados, arquivo, indent=4)
