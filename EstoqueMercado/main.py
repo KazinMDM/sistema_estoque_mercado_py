71f4c187b4879ffb8f3e45006436a81c33ce685b
Lopez <135629199+KazinMDM@users.noreply.github.com>
2025-10-24 09:20:35 -0300

    Update main.py

--- EstoqueMercado/main.py
+++ EstoqueMercado/main.py
@@ -1,13 +1,25 @@
+import time
+import json
+import os
+from utils import Utils
+
 def menu_principal():
-    opcoes = ["Inserir produtos", "Remover produtos", "Atualizar informações do produtos", "Visulizar Estoque", "Buscar produto", "Sair"]
-    print("Olá, seja bem vindo ao Sistema de Estoque de Mercado!")
-    for i in range(len(opcoes)):
-        if opcoes[i] == "Sair":
-            print(f"0 - {opcoes[i]}")
-        else:
-            print(f"{i+1} - {opcoes[i]}")
-    resp = int(input("Digite a opção desejada: "))
-    return resp
+    while True:
+        opcoes = ["Inserir produtos", "Remover produtos", "Atualizar informações do produtos", "Visulizar Estoque", "Buscar produto", "Sair"]
+        print("Olá, seja bem vindo ao Sistema de Estoque de Mercado!")
+        for i in range(len(opcoes)):
+            if opcoes[i] == "Sair":
+                print(f"0 - {opcoes[i]}")
+            else:
+                print(f"{i+1} - {opcoes[i]}")
+        resp = int(input("Digite a opção desejada: "))
+        if resp == 0:
+            print("Saindo do sistema...")
+            break
+        elif resp == 1:
+            Utils.limpar_tela()
+
+
 
 if __name__ == '__main__':
     menu_principal() 
